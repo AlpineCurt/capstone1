f2196d46e45b32ad85898c0534dccd795fbae76f
Curt <alpinecurt@gmail.com>
2022-12-08 13:04:50 -0800

    production ready push 5

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///wildcarrot')
+    os.environ.get(('DATABASE_URL').replace("://", "ql://", 1), 'postgresql:///wildcarrot')
 )
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
