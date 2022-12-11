ad066a3ca66efca12ecbe4de1481ed10b86485b8
Curt <34628026+AlpineCurt@users.noreply.github.com>
2022-12-11 13:14:33 -0800

    delete secrets.py

--- secrets_.py
+++
@@ -1,8 +0,0 @@
-flask_secret_key = "emc13OwZ6F"
-jwt_key = "zil9JVfWj9pB"
-
-edamam_app_id = "2b4bf0c3"
-edamam_app_key = "b7db63449d8aef2259fb1681f9fb9a75"
-google_app_password = "anrihnhdggwtwzzy"
-
-heroku_base_url = 'https://wildcarrot.herokuapp.com'
